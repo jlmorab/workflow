08ec95ac746a1417583242011da2a203124af1ad
Jose Luis Mora <jlmorab@hotmail.com>
2015-09-04 18:39:01 -0500

    Definición de base de datos > Importación Masiva

---
+++ 04 Desarrollo/Solucion/Workflow_Solucion/Workflow_Framework/Infra/INF_Archive.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Workflow.Framework.Infra
+{
+    public class INF_Archive : ICloneable
+    {
+        private string strNombreCompleto;
+        private string strNombre;
+        private string strRuta;
+        private string strPrefijo;
+        private string strSufijo;
+        private string strExtension;
+        private string strProceso;
+
+        #region Constructor
+
+        public INF_Archive() { }
+
+        public INF_Archive(string RutaArchivo) { }
+
+        public INF_Archive(string RutaArchivo, int Prefijo) { }
+
+        public INF_Archive(string RutaArchivo, int Prefijo, int Sufijo) { }
+
+        public INF_Archive(string RutaArchivo, int Prefijo = -1, int Sufijo = -1, char Separador = '\\') { }
+
+        #endregion
+
+        #region Propiedades
+
+        #endregion
+
+        #region Listas
+
+        #endregion
+
+        #region MetodosPublicos
+
+        #endregion
+
+        #region MetodosPrivados
+
+        #endregion
+    }
+}
