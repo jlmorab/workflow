7760dd9b03892174f9cc73b7ecb820737805a616
José Luis Mora <jlmorab@hotmail.com>
2015-09-09 11:51:15 -0500

    Edición de clase INF_Archive > Término de edición de clase

--- 04 Desarrollo/Solucion/Workflow_Solucion/Workflow_Framework/Infra/INF_Archive.cs
+++ 04 Desarrollo/Solucion/Workflow_Solucion/Workflow_Framework/Infra/INF_Archive.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 namespace Workflow.Framework.Infra
 {
-    public class INF_Archive : ICloneable
+    public class INF_Archive
     {
         private string strNombreCompleto;
         private string strNombre;
@@ -15,18 +15,34 @@ namespace Workflow.Framework.Infra
         private string strSufijo;
         private string strExtension;
         private string strProceso;
+        private string strSeparador;
+
+        private int intPrefijo;
+        private int intSufijo;
 
         #region Constructor
 
         public INF_Archive() { }
 
-        public INF_Archive(string RutaArchivo) { }
+        public INF_Archive(string RutaArchivo) 
+        {
+            ObtenerParametros(RutaArchivo, -1, -1, null);
+        }
 
-        public INF_Archive(string RutaArchivo, int Prefijo) { }
+        public INF_Archive(string RutaArchivo, int Prefijo) 
+        {
+            ObtenerParametros(RutaArchivo, Prefijo, -1, null);
+        }
 
-        public INF_Archive(string RutaArchivo, int Prefijo, int Sufijo) { }
+        public INF_Archive(string RutaArchivo, int Prefijo, int Sufijo) 
+        {
+            ObtenerParametros(RutaArchivo, Prefijo, Sufijo, null);
+        }
 
-        public INF_Archive(string RutaArchivo, int Prefijo = -1, int Sufijo = -1, char Separador = '\\') { }
+        public INF_Archive(string RutaArchivo, int Prefijo = -1, int Sufijo = -1, string Separador = null) 
+        { 
+            ObtenerParametros(RutaArchivo, Prefijo, Sufijo, Separador);
+        }
 
         #endregion
 
@@ -44,16 +60,48 @@ namespace Workflow.Framework.Infra
             set { strNombre = value; } 
         }
 
-        public string Ruta 
+        public string Ruta
         {
             get { return strRuta; }
-            set { strRuta = value; }
+            set 
+            {
+                ObtenerParametros(value, -1, -1, null);
+                strRuta = value; 
+            }
         }
 
+        public int CaracteresPrefijo
+        {
+            get { return intPrefijo; }
+            set 
+            { 
+                intPrefijo = value;
+
+                if ((strNombre != null) && (strNombre.Trim() != ""))
+                {
+                    ObtencionPorCaracteres(strNombre, intPrefijo, true, false);
+                }
+            }
+        }
+        
         public string Prefijo 
         {
             get { return strPrefijo; }
             set { strPrefijo = value; } 
+        }
+
+        public int CaracteresSufijo
+        {
+            get { return intSufijo; }
+            set 
+            { 
+                intSufijo = value;
+
+                if ((strNombre != null) && (strNombre.Trim() != ""))
+                {
+                    ObtencionPorCaracteres(strNombre, intSufijo, false, true);
+                }
+            }
         }
 
         public string Sufijo 
@@ -82,11 +130,19 @@ namespace Workflow.Framework.Infra
 
         #region MetodosPublicos
 
+        public void AplicarSeparador(string Separador, bool Prefijo, bool Sufijo)
+        {
+            if ((Nombre != null) && (Nombre.Trim() != ""))
+            {
+                ObtencionPorSeparador(Nombre, Separador, Prefijo, Sufijo);
+            }
+        }
+
         #endregion
 
         #region MetodosPrivados
 
-        private void ObtenerParametros(string Ruta)
+        private void ObtenerParametros(string Ruta, int Prefijo = -1, int Sufijo = -1, string Separador = "_")
         {
             // Nombre Completo
             strNombreCompleto = Path.GetFileName(Ruta);
@@ -101,15 +157,84 @@ namespace Workflow.Framework.Infra
             strRuta = Path.GetDirectoryName(Ruta);
         }
 
-        private void ObtenerPrefijos(string Archivo, int Caracteres, string Separador = null)
+        private void ObtenerPrefijos(string Archivo, int Caracteres, string Separador = "_")
         {
+            // Incializa valores
+            strPrefijo = "";
+            
             if (Separador != null)
             {
+                ObtencionPorSeparador(Archivo, Separador, true, false);
+            }
+            else
+            {
+                ObtencionPorCaracteres(Archivo, Caracteres, true, false);
+            }
+        }
+
+        private void ObtenerSufijo(string Archivo, int Caracteres, string Separador = "_")
+        {
+            // Incializa valores
+            strSufijo = "";
+
+            if (Separador != null)
+            {
+                ObtencionPorSeparador(Archivo, Separador, false, true);
+            }
+            else
+            {
+                ObtencionPorCaracteres(Archivo, Caracteres, false, true);
+            }
+        }
+
+        private void ObtencionPorSeparador(string Archivo, string Separador = "_", bool Prefijo = false, bool Sufijo = false)
+        {   
+            // Prefijo
+            if (Prefijo)
+            {
+                // Obtiene posición del separador
                 int pos = Archivo.IndexOf(Separador);
 
                 if (pos - 1 >= 0)
                 {
-                    strPrefijo = Archivo.Substring(0,pos - 1);
+                    // Obtiene cadena de caracteres anteriores al separador
+                    strPrefijo = Archivo.Substring(0, pos - 1).ToUpper();
+                }
+            }
+
+            // Sufijo
+            if (Sufijo)
+            {
+                // Obtiene última posición del separador
+                int pos = Archivo.LastIndexOf(Separador);
+
+                if (pos - 1 >= 0)
+                {
+                    // Obtiene cadena de caracteres posteriores al separador
+                    strSufijo = Archivo.Substring(pos + 1).ToUpper();
+                }
+            }
+        }
+
+        private void ObtencionPorCaracteres(string Archivo, int Caracteres, bool Prefijo = false, bool Sufijo = false)
+        {
+            if (Caracteres > 0)
+            {
+                // Comprueba que el archivo tenga más de los caracteres definidos
+                if (Archivo.Length > Caracteres)
+                {
+                    // Obtiene cadena de caracteres de acuerdo a caracteres definidos
+                    // Prefijo
+                    if (Prefijo)
+                    {
+                        strPrefijo = Archivo.Substring(0, Caracteres).ToUpper();
+                    }
+
+                    // Sufijo
+                    if (Sufijo)
+                    {
+                        strSufijo = Archivo.Substring(Archivo.Length - Caracteres).ToUpper();
+                    }
                 }
             }
         }
