b914eb9f3eb332d50d984be0426721539df91d18
José Luis Mora <jlmorab@hotmail.com>
2015-10-13 17:17:47 -0500

    Importacion Masiva > Se agregan querys de los stored procedured y funciones utilizadas en la capa de datos, en base de datos DB2

---
+++ 04 Desarrollo/Base de Datos/SEL_LAYOUT_20150914.sql
@@ -0,0 +1,33 @@
+--  Generate SQL 
+--  Version:                   	V5R4M0 060210 
+--  Generated on:              	14/09/15 15:18:27 
+--  Relational Database:       	B103BE2F 
+--  Standards Option:          	DB2 UDB iSeries 
+  
+SET PATH "QSYS","QSYS2","QGPL","CARVALHORA" ; 
+  
+CREATE PROCEDURE CARVALHORA.SEL_LAYOUT ( 
+	IN P_ID_LAYOUT INT ) 
+	DYNAMIC RESULT SETS 1 
+	LANGUAGE SQL 
+	SPECIFIC CARVALHORA.SEL_LAYOUT 
+	NOT DETERMINISTIC 
+	READS SQL DATA 
+	CALLED ON NULL INPUT 
+	SET OPTION  ALWBLK = *ALLREAD , 
+	ALWCPYDTA = *OPTIMIZE , 
+	COMMIT = *NONE , 
+	DECRESULT = (31, 31, 00) , 
+	DFTRDBCOL = *NONE , 
+	DYNDFTCOL = *NO , 
+	DYNUSRPRF = *USER , 
+	SRTSEQ = *HEX   
+	BEGIN 
+		P1 : BEGIN 
+  
+					DECLARE C1 CURSOR WITH RETURN FOR 
+						SELECT * FROM CARVALHORA . CAT_LAYO WHERE LAY_CVE = P_ID_LAYOUT ;
+					OPEN C1 ; 
+					END P1 ; 
+  
+		END  ;
